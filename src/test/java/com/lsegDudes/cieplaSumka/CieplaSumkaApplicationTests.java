56bdae3b19228359b039bf52b5702c662b7b8568
Artur Ciepielowski <artciepi@gmail.com>
2023-03-25 09:41:20 +0100

    First commit

---
+++ src/test/java/com/lsegDudes/cieplaSumka/CieplaSumkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsegDudes.cieplaSumka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CieplaSumkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
