aae577999129975a0b2a6e07ca09c5f40ef9cec1
Artur Ciepielowski <artciepi@gmail.com>
2023-03-27 09:13:10 +0200

    Still trying to figure out this broken tut

--- src/main/java/com/lsegDudes/cieplaSumka/config/JwtAthFilter.java
+++ src/main/java/com/lsegDudes/cieplaSumka/config/JwtAthFilter.java
@@ -4,9 +4,13 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 public class JwtAthFilter extends OncePerRequestFilter {
     @Override
@@ -18,7 +22,13 @@ public class JwtAthFilter extends OncePerRequestFilter {
         final String jwtToken;
 
         if (authHeader == null || !authHeader.startsWith("Bearer")){
-
+        filterChain.doFilter(request,response);
+        return;
+        }
+        jwtToken=authHeader.substring(7);
+        userEmail="something"; //TODO to be implementet
+        if(userEmail !=null && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails = userDetailsService.loadUserByUsername(userEmial);
         }
     }
 }
